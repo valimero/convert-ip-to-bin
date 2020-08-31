3614fa2594ca6394157354d0e54f4dd67cdf08bf
Valentin Coppin <valentin@socode.fr>
2020-08-31 14:19:03 +0200

    Add the number of available addresses

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ def convertIntToBin8(n):
 	return f'{n:08b}'
 
 def inputIpAdress():
-	return input("Ip adresss to convert:");
+	return input("Ip adresss to convert:")
+
+def reverseBin(adressBin: str):
+	return adressBin.replace('0', 'b').replace('1', 'a').replace('a', '0').replace('b', '1')
 
 # Convert Ip adress to bin
 def convertIpAdressToBin(ipAddress: str):
@@ -21,10 +24,17 @@ def convertIpAdressToBin(ipAddress: str):
 
 	return r
 
+def getNumberPossibleAddress(ipAdressConverted : str):
+	return 2 ** ipAdressConverted.count('0') - 2
 
-def diplayResult(ipAdress: str, ipAdressConverted: str):
-	print("---------------------------------------------------\n Ip address : \t" + ipAdress + "\n Result: \t" + ipAdressConverted)
-	print("---------------------------------------------------")
+
+def diplayResult(ipAdress: str, ipAdressConverted: str, numberPossibleAddress: int):
+	print("-----------------------------------------------------------")
+	print("Subnet mask : \t\t" + ipAdress)
+	print("Subnet mask bin: \t" + ipAdressConverted)
+	print()
+	print("Number of available addresses: " + str(numberPossibleAddress))
+	print("-----------------------------------------------------------")
 
 
 argv = sys.argv
@@ -34,5 +44,10 @@ if(len(argv) > 1):
 else:
 	inputIpAdress = inputIpAdress()
 
+# Convert the subnet mask to binary
+ipAdressConverted = convertIpAdressToBin(inputIpAdress)
 
-diplayResult(inputIpAdress, convertIpAdressToBin(inputIpAdress))
+# Get the number of available addresses
+numberPossibleAddress = getNumberPossibleAddress(ipAdressConverted)
+
+diplayResult(inputIpAdress, ipAdressConverted, numberPossibleAddress)
